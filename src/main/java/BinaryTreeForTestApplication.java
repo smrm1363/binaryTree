a2eef5a31e1e1700900648ef075fd89d6d6b9e95
Mohamadreza Mirali <smrm1363@gmail.com>
2019-01-20 16:46:47 +0330

    may be correct

--- src/main/java/BinaryTreeForTestApplication.java
+++ src/main/java/BinaryTreeForTestApplication.java
@@ -7,20 +7,20 @@ public class BinaryTreeForTestApplication {
     public static void main(String[] args) {
         System.out.println("Hiiiiii");
         Node tree = new Node((new NodeData(null,10)));
-
-        tree.setLeft(new Node(new NodeData(null,5)));
-        tree.setRight(new Node(new NodeData("Ali",2)));
-        tree.getLeft().setRight(new Node(new NodeData("Mammad",2)));
-     //   tree.printTree(tree.getRoot());
-
-        System.out.println("other tree :");
-        tree.print2DTree(tree);
+//
+//        tree.setLeft(new Node(new NodeData(null,5)));
+//        tree.setRight(new Node(new NodeData("Ali",2)));
+//        tree.getLeft().setRight(new Node(new NodeData("Mammad",2)));
+//     //   tree.printTree(tree.getRoot());
+//
+//        System.out.println("other tree :");
+//        tree.print2DTree(tree);
 
         Integer x = null;
         System.out.println("............."+x+1);
 
         FileReader fileReader = FileReader.getInstance();
-        tree = tree.generateTree(fileReader.readFileWordCount("C:\\Users\\hurie&mohamadreza\\Downloads\\input001.txt"));
+        tree = tree.generateTree(fileReader.readFileWordCount("E:\\projects\\myself\\Germany\\TestData.txt"));
         tree.print2DTree(tree);
         System.out.println("****");
 
